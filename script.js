94f0d7ab80421971fe5bb8961e37fe9e6822e30d
Mark <mariusureche1@gmail.com>
2020-04-07 14:04:34 +0200

    This is my first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+var number = 5;
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[2]);
+
+// alert("Hello World!");
+
+//let answer = confirm("Are you here?");
+
+// console.log(answer)
+
+let answer = prompt("Are you 18?", "Yes");
+
+console.log(typeof(null));
